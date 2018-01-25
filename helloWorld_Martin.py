9eef1aac7ee4d1c041188bf6754718737e6b74a3
CPSC2 <35793268+CPSC2@users.noreply.github.com>
2018-01-25 02:41:33 -0600

    Add files via upload

---
+++ helloWorld_Martin.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+# Name: Michael Martin
+# Date: 1.22.2017
+# Assignment:
+
+# This assignment you are to create a python program that outputs :
+#" Hello Programming World - Michael Lewis". Instead of adding my name -
+#Michael Lewis, add your name. Also the output should be the product
+#of adding two strings together,  Hellow Programming World and Your name .
+#This is worth 10 points. Submit your program to github classroom at link :
+#https://classroom.github.com/a/q8pjhdaM
+
+first_name = "Mike"
+last_name = "Martin"
+print("Hello Programming World - ", first_name, last_name)
+
+
+# main():
+    # print()
+
+
+# if __main__ == __name__:main()
+
